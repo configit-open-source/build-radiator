4c3c70b661a33df58d2bede1fc6b5ee6f3694d29
Andrew Butler <andrew@configit.com>
2016-05-06 14:38:30 +0100

    Update Angular to 1.5

--- BuildRadiator/Client/Directives/BuildStatusIcon.js
+++ BuildRadiator/Client/Directives/BuildStatusIcon.js
@@ -2,14 +2,19 @@
   'use strict';
 
   var module = angular.module( 'BuildRadiator' );
-  
+
   module.directive( 'buildStatusIcon', function() {
     return {
       restrict: 'E',
       scope: {
         status: '='
       },
-      template: '<md-icon md-svg-src="Content/icons/{{ status }}.svg" alt="{{ status }}"></md-icon>'
+      template: '<md-icon md-svg-src="{{ iconUrl }}" alt="{{ status }}"></md-icon>',
+      link: function( scope ) {
+        scope.$watch( 'status', function() {
+          scope.iconUrl = scope.status ? 'Content/icons/' + scope.status + '.svg' : null;
+        } );
+      }
     };
   } );
 
