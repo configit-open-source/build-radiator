a5f095aeb408bbc183fa3881258c7cdd2a9c0719
Andrew Butler <andrew@configit.com>
2017-02-03 10:29:18 +0000

    Updated tiles

--- BuildRadiator/Controllers/TileController.cs
+++ BuildRadiator/Controllers/TileController.cs
@@ -9,18 +9,18 @@ namespace Configit.BuildRadiator.Controllers {
 
     static TileController() {
       Tiles = new Tile[] {
-        new ProjectTile( "Ace Commit", "Ace Commit" ) { ColumnSpan = 2, RowSpan = 2 },
+        new ProjectTile( "Ace (master)", "Ace Commit" ) { ColumnSpan = 2, RowSpan = 2 },
         new MessageTile( "Current Theme", "sprintTheme", "fancy" ) { ColumnSpan = 2 }, 
         new ClockTile( "UK Time", "Europe/London" ),
-        new ProjectTile( "Ngyn Commit", "Ngyn Commit" ),
-        new ProjectTile( "Vcdb Commit", "Vcdb Commit" ),
-        new ProjectTile( "Installer Commit", "Installer Commit" ),
-        new ProjectTile( "Grid Commit", "Grid Commit" ),
-        new ProjectTile( "Base Commit", "Base Commit" ),
+        new ProjectTile( "Ngyn", "Ngyn Commit" ),
+        new ProjectTile( "Vcdb", "Vcdb Commit" ),
+        new ProjectTile( "Installer", "Installer Commit" ),
+        new ProjectTile( "Grid", "Grid Commit" ),
         new ProjectTile( "Database Installer", "Database Installer Commit" ),
-        new ProjectTile( "Ace Carbon", "Ace Commit (NUnit2)" ),
-        new ProjectTile( "Ace Daily (JLR)", "Ace Daily Deploy (JLR)" ),
         new ProjectTile( "Ace Daily (Product)", "Ace Daily Deploy" ),
+        new ProjectTile( "Ace Daily (John Deere)", "Ace Daily Deploy (John Deere)" ),
+        new ProjectTile( "Ace Daily (JLR)", "Ace Daily Deploy (JLR)" ),
+        new ProjectTile( "Ace (carbon)", "Ace Commit (NUnit2)" ),
         new ProjectTile( "Ace End To End", "Ace End To End Test" ),
         new ProjectTile( "Ace Upgrade", "Ace Daily Upgrade" )
       };
