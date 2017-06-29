104ec5dc297288ba950afcb16f80443fe520fd83
JamesWebb73 <jwebb@configit.com>
2017-06-29 10:54:49 +0100

    wip build tiles working i think

--- BuildRadiator/Client/Controllers/DashboardController.js
+++ BuildRadiator/Client/Controllers/DashboardController.js
@@ -1,52 +1,52 @@
-﻿( function() {
+﻿(function () {
   'use strict';
 
-  var module = angular.module( 'BuildRadiator' );
+  var module = angular.module('BuildRadiator');
 
-  module.controller( 'DashboardController', ['$scope', '$log', '$sce', 'TileConfiguration', 'BuildHub', 'MessageHub', function( $scope, $log, $sce, TileConfiguration, BuildHub, MessageHub ) {
+  module.controller('DashboardController', ['$scope', '$log', '$sce', 'TileConfiguration', 'BuildHub', 'MessageHub', function ($scope, $log, $sce, TileConfiguration, BuildHub, MessageHub) {
     var self = this;
 
     self.committerLimit = 11;
 
-    function onMessageUpdate( message ) {
-      var tile = self.tiles.find( function( tile ) {
+    function onMessageUpdate(message) {
+      var tile = self.tiles.find(function (tile) {
         return tile.type === 'Configit.BuildRadiator.Model.MessageTile'
           && tile.messageKey === message.key;
-      } );
+      });
 
-      if ( !tile ) {
+      if (!tile) {
         return;
       }
 
-      message.contentHtml = $sce.trustAsHtml( message.content );
+      message.contentHtml = $sce.trustAsHtml(message.content);
 
       tile.message = message;
     };
 
-    function onProjectUpdateError( build ) {
-      var tile = self.tiles.find( function( tile ) {
+    function onProjectUpdateError(build) {
+      var tile = self.tiles.find(function (tile) {
         return tile.type === 'Configit.BuildRadiator.Model.BuildTile'
           && tile.build.name === build.name
           && tile.build.branchName === build.branchName;
-      } );
+      });
 
-      if ( !tile ) {
-        $log.error( 'UNKNOWN PROJECT', build );
+      if (!tile) {
+        $log.error('UNKNOWN PROJECT', build);
         return;
       }
 
       tile.error = build.error;
     };
 
-    function onProjectUpdate( build ) {
-      var tile = self.tiles.find( function( tile ) {
-          return tile.type === 'Configit.BuildRadiator.Model.BuildTile' && tile.build.id === build.buildId;
-          //&& tile.build.name === build.name
-          //&& tile.build.branchName === build.branchName;
-      } );
+    function onProjectUpdate(build) {
+      var tile = self.tiles.find(function (tile) {
+        return tile.type === 'Configit.BuildRadiator.Model.BuildTile' 
+        && tile.build.name === build.name
+        && tile.build.branchName === build.branchName;
+      });
 
-      if ( !tile ) {
-        $log.error( 'UNKNOWN PROJECT', build );
+      if (!tile) {
+        $log.error('UNKNOWN PROJECT', build);
         return;
       }
 
@@ -55,33 +55,33 @@
     }
 
     function registerProjects() {
-      self.tiles.filter( function( t ) {
+      self.tiles.filter(function (t) {
         return t.type === 'Configit.BuildRadiator.Model.BuildTile';
-      } ).forEach( function( tile ) {
-        BuildHub.server.register( tile.build.name, tile.build.branchName );
-      } );
+      }).forEach(function (tile) {
+        BuildHub.server.register(tile.build.name, tile.build.branchName);
+      });
     }
 
     function registerMessages() {
-      self.tiles.filter( function( t ) {
+      self.tiles.filter(function (t) {
         return t.type === 'Configit.BuildRadiator.Model.MessageTile';
-      } ).forEach( function( tile ) {
-        MessageHub.server.get( tile.messageKey );
-      } );
+      }).forEach(function (tile) {
+        MessageHub.server.get(tile.messageKey);
+      });
     }
 
-    TileConfiguration.get().then( function( tiles ) {
+    TileConfiguration.get().then(function (tiles) {
       self.tiles = tiles;
 
-      BuildHub.connect( $scope, {
+      BuildHub.connect($scope, {
         update: onProjectUpdate,
         updateError: onProjectUpdateError
-      } ).done( registerProjects );
+      }).done(registerProjects);
 
-      MessageHub.connect( $scope, {
+      MessageHub.connect($scope, {
         update: onMessageUpdate
-      } ).done( registerMessages );
-    } );
-  }] );
+      }).done(registerMessages);
+    });
+  }]);
 
-} )();
+})();
--- BuildRadiator/Helpers/BuildService.cs
+++ BuildRadiator/Helpers/BuildService.cs
@@ -7,94 +7,83 @@ using System.Xml;
 
 using Configit.BuildRadiator.Model.Builds;
 
-namespace Configit.BuildRadiator.Helpers
-{
-    public class BuildService
-    {
-        public const string DefaultBranchName = "";//"<default>";
+namespace Configit.BuildRadiator.Helpers {
+  public class BuildService {
+    public const string DefaultBranchName = "";//"<default>";
 
-        private readonly string _baseUrl;
-        private readonly string _authenticationHeader;
+    private readonly string _baseUrl;
+    private readonly string _authenticationHeader;
 
-        public BuildService(string baseUrl, string authenticationHeader)
-        {
-            _baseUrl = baseUrl.TrimEnd('/');
-            _authenticationHeader = authenticationHeader;
-        }
-
-        private HttpClient CreateClient()
-        {
-            var client = new HttpClient
-            {
-                BaseAddress = new Uri($"{_baseUrl}/httpAuth/app/rest/latest/")
-            };
-
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _authenticationHeader);
-
-            return client;
-        }
-
-        public async Task<Build> Get(string buildType, string branchName)
-        {
-            var client = CreateClient();
-
-            var buildInfoTask = Task.Run(() => GetBuildInfo(client, buildType, branchName));
-            var investigationInfoTask = Task.Run(() => GetInvestigationInfo(client, buildType));
-            var changesSinceFailureInfoTask = Task.Run(() => GetChangesSinceFailureInfo(client, buildType, branchName));
-
-            await Task.WhenAll(buildInfoTask, investigationInfoTask, changesSinceFailureInfoTask);
-
-            var parser = new BuildParser(buildInfoTask.Result, investigationInfoTask.Result, changesSinceFailureInfoTask.Result);
-
-            return parser.Parse();
-        }
-
-        private static async Task<XmlDocument> GetBuildInfo(HttpClient client, string buildType, string branchName)
-        {
-            var branch = string.IsNullOrEmpty(branchName) ? "" : $"branch:{Uri.EscapeUriString(branchName)},";
-
-            var url = "builds/buildType:name:" + Uri.EscapeDataString(buildType) + ",running:any," + branch + "count:1?fields=defaultBranch,buildType,branchName,status,statusText,startDate,finishDate,running,running-info";
-            var cacheBuster = "&t=" + DateTime.UtcNow.Ticks;
-
-            var data = await client.GetStringAsync(url + cacheBuster);
-
-            var document = new XmlDocument();
-            document.LoadXml(data);
-
-            return document;
-        }
-
-        private static async Task<XmlDocument> GetInvestigationInfo(HttpClient client, string buildType)
-        {
-            var url = "investigations?locator=buildType:name:" + Uri.EscapeDataString(buildType) + "&fields=investigation(state,assignee(name,email),assignment(text,user(name,email)))";
-            var cacheBuster = "&t=" + DateTime.UtcNow.Ticks;
-
-            var data = await client.GetStringAsync(url + cacheBuster);
-
-            var document = new XmlDocument();
-            document.LoadXml(data);
-
-            return document;
-        }
-
-        private static async Task<XmlDocument> GetChangesSinceFailureInfo(HttpClient client, string buildType, string branchName)
-        {
-            var buildLocator = "buildType:name:" + Uri.EscapeDataString(buildType) + ",branch:" + Uri.EscapeDataString(branchName);
-            var url = "builds/?locator=" + buildLocator + ",status:failure,running:false,sinceBuild:(" + buildLocator + ",status:success,running:false)&fields=build(changes(change(username,user(email))))";
-            var cacheBuster = "&t=" + DateTime.UtcNow.Ticks;
-
-            var document = new XmlDocument();
-            try
-            {
-                var data = await client.GetStringAsync(url + cacheBuster);
-                document.LoadXml(data);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Could not load changes, probably because there are no successful builds {0}", ex.Message);
-            }
-            return document;
-
-        }
+    public BuildService( string baseUrl, string authenticationHeader ) {
+      _baseUrl = baseUrl.TrimEnd( '/' );
+      _authenticationHeader = authenticationHeader;
     }
+
+    private HttpClient CreateClient() {
+      var client = new HttpClient {
+        BaseAddress = new Uri( $"{_baseUrl}/httpAuth/app/rest/latest/" )
+      };
+
+      client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue( "Basic", _authenticationHeader );
+
+      return client;
+    }
+
+    public async Task<Build> Get( string buildType, string branchName ) {
+      var client = CreateClient();
+
+      var buildInfoTask = Task.Run( () => GetBuildInfo( client, buildType, branchName ) );
+      var investigationInfoTask = Task.Run( () => GetInvestigationInfo( client, buildType ) );
+      var changesSinceFailureInfoTask = Task.Run( () => GetChangesSinceFailureInfo( client, buildType, branchName ) );
+
+      await Task.WhenAll( buildInfoTask, investigationInfoTask, changesSinceFailureInfoTask );
+
+      var parser = new BuildParser( buildInfoTask.Result, investigationInfoTask.Result, changesSinceFailureInfoTask.Result );
+
+      return parser.Parse();
+    }
+
+    private static async Task<XmlDocument> GetBuildInfo( HttpClient client, string buildType, string branchName ) {
+      var branch = string.IsNullOrEmpty( branchName ) ? "" : $"branch:{Uri.EscapeUriString( branchName )},";
+
+      var url = "builds/buildType:name:" + Uri.EscapeDataString( buildType ) + ",running:any," + branch + "count:1?fields=defaultBranch,buildType,branchName,status,statusText,startDate,finishDate,running,running-info";
+      var cacheBuster = "&t=" + DateTime.UtcNow.Ticks;
+
+      var data = await client.GetStringAsync( url + cacheBuster );
+
+      var document = new XmlDocument();
+      document.LoadXml( data );
+
+      return document;
+    }
+
+    private static async Task<XmlDocument> GetInvestigationInfo( HttpClient client, string buildType ) {
+      var url = "investigations?locator=buildType:name:" + Uri.EscapeDataString( buildType ) + "&fields=investigation(state,assignee(name,email),assignment(text,user(name,email)))";
+      var cacheBuster = "&t=" + DateTime.UtcNow.Ticks;
+
+      var data = await client.GetStringAsync( url + cacheBuster );
+
+      var document = new XmlDocument();
+      document.LoadXml( data );
+
+      return document;
+    }
+
+    private static async Task<XmlDocument> GetChangesSinceFailureInfo( HttpClient client, string buildType, string branchName ) {
+      var buildLocator = "buildType:name:" + Uri.EscapeDataString( buildType ) + ",branch:" + Uri.EscapeDataString( branchName );
+      var url = "builds/?locator=" + buildLocator + ",status:failure,running:false,sinceBuild:(" + buildLocator + ",status:success,running:false)&fields=build(changes(change(username,user(email))))";
+      var cacheBuster = "&t=" + DateTime.UtcNow.Ticks;
+
+      var document = new XmlDocument();
+      try {
+        var data = await client.GetStringAsync( url + cacheBuster );
+        document.LoadXml( data );
+      }
+      catch ( Exception ex ) {
+        Debug.WriteLine( "Could not load changes, probably because there are no successful builds {0}", ex.Message );
+      }
+      return document;
+
+    }
+  }
 }
