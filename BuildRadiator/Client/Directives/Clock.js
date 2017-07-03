8bce45ed2034ec62fac8fbe82e657a02b4bf3ed7
JamesWebb73 <jwebb@configit.com>
2017-07-03 13:05:33 +0100

    wip

--- BuildRadiator/Client/Directives/Clock.js
+++ BuildRadiator/Client/Directives/Clock.js
@@ -3,33 +3,30 @@
 
   var module = angular.module( 'BuildRadiator' );
 
-  module.directive( 'clock', ['$interval', '$sce', function( $interval, $sce ) {
+  module.directive( 'clock', ['$interval', function( $interval, $sce ) {
     return {
       restrict: 'E',
       scope: {
-        timezone: '=',
-        title: '='
+        
+        title: '=',
+        timezone: '='
       },
     
     template: '<div flex ng-if="!tile.error" class="message-content">'
-           +  '<div> {{ time }}</div><div class="small">{{ date }}</div><div class="small" ></div>'
-           +  '</div>'
-    + '<md-grid-tile-footer ><h3><input value="{{ title }}" /><select ngyn-select2 class="concealed-input" ng-model="timezones"></select></h3></md-grid-tile-footer>',
+      + '<div> {{ time }}</div><div class="small">{{ date }}</div><div class="small" ></div>'
+      + '</div>'
+      //+ '<div class="small">{{ timezone }}<div>'
+      + '<md-grid-tile-footer ><h3><input value="{{ title }}" /><select class="concealed-input" ng-model="timezone" ng-options="t for t in timezoneList"/></h3></md-grid-tile-footer>',
 
       link: function( scope ) {
         function refresh() {
           var now = moment().tz( scope.timezone );
-         // var timezoneList = '<option selected="1">' + scope.timezone + '</option>';
-
-          scope.time = now.format( 'HH:mm' );
+          scope.time = now.format( 'HH:mm:ss' );
           scope.date = now.format( 'DD MMMM YYYY' );
-
-          scope.timezoneList = '<option selected="1">' + scope.timezone + '</option>';
-          moment.tz.names().forEach( zoneOption );
-
-          scope.timezones = $sce.trustAsHtml( scope.timezoneList );
+          
         }
 
+        scope.timezoneList = moment.tz.names();
         var timer = $interval( refresh, 1000 );
 
         scope.$on( '$destroy', function() {
@@ -37,10 +34,6 @@
         } );
 
         refresh();
-
-        function zoneOption( item, index ) {
-          scope.timezoneList += '<option>' + item + '</option>';
-        }
       }
     };
   }] );
