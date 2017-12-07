b547338bf6d7f490c4a5624db8468d1577ceed51
Andrew Butler <andrew@configit.com>
2017-12-07 11:40:30 +0000

    [Tiles] Added support for Dual Project tiles * Shows two builds on one tile (used for commit & daily deploy)

--- BuildRadiator/Client/Controllers/DashboardController.js
+++ BuildRadiator/Client/Controllers/DashboardController.js
@@ -9,49 +9,93 @@
     ctrl.committerLimit = 11;
 
     function onMessageUpdate( message ) {
-      var tile = ctrl.tiles.find( function( tile ) {
+      var tiles = ctrl.tiles.filter( function( tile ) {
         return tile.type === 'message'
           && tile.config.messageKey === message.key;
       } );
 
-      if ( !tile ) {
-        return;
+      if ( tiles.length ) {
+        message.contentHtml = $sce.trustAsHtml( message.content );
       }
 
-      message.contentHtml = $sce.trustAsHtml( message.content );
-
-      tile.message = message;
+      tiles.forEach( function( t ) {
+        t.message = message;  
+      } );
     };
 
     function onProjectUpdateError( build ) {
-      var tile = ctrl.tiles.find( function( tile ) {
+      var projectTiles = ctrl.tiles.filter( function( tile ) {
         return tile.type === 'project'
           && tile.config.buildId === build.buildId
           && tile.config.branchName === build.branchName;
       } );
 
-      if ( !tile ) {
-        $log.error( 'UNKNOWN PROJECT', build );
-        return;
-      }
+      projectTiles.forEach( function( t ) {
+        t.error = build.error;
+      } );
+      
+      var primaryProjectTiles = ctrl.tiles.filter( function( tile ) {
+        return tile.type === 'dual-project'
+          && tile.config.primaryBuildId === build.buildId
+          && tile.config.primaryBranchName === build.branchName;
+      } );
 
-      tile.error = build.error;
+      primaryProjectTiles.forEach( function( t ) {
+        t.primaryError = build.error;
+      } );
+      
+      var secondaryProjectTiles = ctrl.tiles.filter( function( tile ) {
+        return tile.type === 'dual-project'
+          && tile.config.secondaryBuildId === build.BuildId
+          && tile.config.secondaryBranchName === build.branchName;
+      } );
+
+      secondaryProjectTiles.forEach( function( t ) {
+        t.secondaryError = build.error;
+      } );
+
+      if ( !projectTiles.length && !primaryProjectTiles.length && !secondaryProjectTiles.length ) {
+        $log.error( 'onProjectError: UNKNOWN PROJECT', build );
+      }
     };
 
     function onProjectUpdate( build ) {
-      var tile = ctrl.tiles.find( function( tile ) {
+      var projectTiles = ctrl.tiles.filter( function( tile ) {
         return tile.type === 'project'
           && tile.config.buildId === build.id
           && tile.config.branchName === build.branchName;
       } );
 
-      if ( !tile ) {
-        $log.error( 'UNKNOWN PROJECT', build );
-        return;
-      }
+      projectTiles.forEach( function( t ) {
+        t.project = build;
+        delete t.error;
+      } );
+      
+      var primaryProjectTiles = ctrl.tiles.filter( function( tile ) {
+        return tile.type === 'dual-project'
+          && tile.config.primaryBuildId === build.id
+          && tile.config.primaryBranchName === build.branchName;
+      } );
 
-      tile.project = build;
-      delete tile.error;
+      primaryProjectTiles.forEach( function( t ) {
+        t.primaryProject = build;
+        delete t.primaryError;
+      } );
+      
+      var secondaryProjectTiles = ctrl.tiles.filter( function( tile ) {
+        return tile.type === 'dual-project'
+          && tile.config.secondaryBuildId === build.id
+          && tile.config.secondaryBranchName === build.branchName;
+      } );
+
+      secondaryProjectTiles.forEach( function( t ) {
+        t.secondaryProject = build;
+        delete t.secondaryError;
+      } );
+
+      if ( !projectTiles.length && !primaryProjectTiles.length && !secondaryProjectTiles.length ) {
+        $log.error( 'onProjectUpdate: UNKNOWN PROJECT', build );
+      }
     };
 
     function registerProjects() {
@@ -59,6 +103,13 @@
         return t.type === 'project';
       } ).forEach( function( tile ) {
         BuildHub.server.register( tile.config.buildId, tile.config.branchName );
+      } );
+
+      ctrl.tiles.filter( function( t ) {
+        return t.type === 'dual-project';
+      } ).forEach( function( tile ) {
+        BuildHub.server.register( tile.config.primaryBuildId, tile.config.primaryBranchName );
+        BuildHub.server.register( tile.config.secondaryBuildId, tile.config.secondaryBranchName );
       } );
     }
 
@@ -70,8 +121,13 @@
       } );
     }
 
-    ctrl.gotoProject = function( url ) {
+    ctrl.gotoProject = function( url, e ) {
+      if ( !url ) {
+        return;
+      }
+
       $window.location = url;
+      e.stopPropagation();
     }
 
     TileConfiguration.get().then( function( tiles ) {
---
+++ BuildRadiator/Model/Tiles/DualProjectTile.cs
@@ -0,0 +1,18 @@
+﻿using Configit.BuildRadiator.Helpers;
+
+namespace Configit.BuildRadiator.Model {
+  public class DualProjectTile: Tile<DualProjectTileConfig> {
+    public override string Type => "dual-project";
+
+    public DualProjectTile( string caption, string secondaryCaption, string primaryBuildId, string primaryBranchName, string secondaryBuildId, string secondaryBranchName ) {
+      Caption = caption;
+      Config = new DualProjectTileConfig {
+        SecondaryCaption = secondaryCaption,
+        PrimaryBuildId = primaryBuildId,
+        PrimaryBranchName = !string.IsNullOrWhiteSpace( primaryBranchName ) ? primaryBranchName : BuildService.DefaultBranchName,
+        SecondaryBuildId = secondaryBuildId,
+        SecondaryBranchName = !string.IsNullOrWhiteSpace( secondaryBranchName ) ? secondaryBranchName : BuildService.DefaultBranchName
+      };
+    }
+  }
+}
